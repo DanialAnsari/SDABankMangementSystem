66ef250926e05fb644e6ddea042dac45bbff3b0f
Danial Ansari <danialansari998@gmail.com>
2019-05-12 00:12:15 +0500

    first commit

---
+++ DataAccessTier/Absentee.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessTier
+{
+    class Absentee
+    {
+    }
+
+    class Account { }
+
+    class BankTransaction { }
+
+    class City { }
+
+    class Customer { }
+
+    class Desgination { }
+
+    class Education { }
+
+    class Employee { }
+
+    class Loan { }
+
+    class LoanInstallment { }
+
+    class LoanPurpose { }
+
+    class LoanType { }
+
+    class Locker { }
+
+    class LockerPossesion { }
+
+    class LockerPurpose { }
+
+    class MoneyTransfer { }
+
+    class Person { }
+
+    class PhoneNumber { }
+
+    class Policy { }
+
+    class Repayment { }
+    class Salary { }
+
+    class TransactionType { }
+}
