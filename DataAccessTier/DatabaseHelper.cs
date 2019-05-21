08a9c9d3aa34f70728dd35be038277c50e1f8cdf
02-131162-016 02-131162-016 <02-131162-016@student.bahria.edu.pk>
2019-05-21 10:47:39 +0500

    Project Updated with DB and Data Access layer

---
+++ DataAccessTier/DatabaseHelper.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessTier
+{
+    class DatabaseHelper
+    {
+
+        SqlConnection conn;
+        SqlCommand cmd;
+        SqlDataAdapter da;
+        SqlDataReader dr;
+        DataTable dt;
+
+        public DatabaseHelper()
+        {
+            string connectionString = "Data Source=LAPTOP-N3NU3IOC\\MSSQLSERVER14;Initial Catalog=SDA_BANK_MANAGEMENT_DATABASE;Integrated Security=True";
+            conn = new SqlConnection(connectionString);
+        }
+
+        public DataTable Select(string q)
+        {
+            try
+            {
+
+                cmd = new SqlCommand(q, conn);
+                conn.Open();
+                da = new SqlDataAdapter(cmd);
+                dt = new DataTable();
+                da.Fill(dt);
+                conn.Close();
+                return dt;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            return null;
+        }
+
+        public bool insertUpdateDelete(string query)
+        {
+            try
+            {
+                cmd = new SqlCommand(query, conn);
+                conn.Open();
+                int count = cmd.ExecuteNonQuery();
+                if (count > 0)
+                {
+                    conn.Close();
+                    return true;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+
+            }
+            conn.Close();
+            return false;
+
+        }
+    }
+}
