267b5d38a61cdd2b0e9f27f92c2d5020ba02c27c
02-131162-016 02-131162-016 <02-131162-016@student.bahria.edu.pk>
2019-05-27 23:02:33 +0500

    Commits#2

--- DataAccessTier/Absentee.cs
+++ DataAccessTier/Absentee.cs
@@ -1,11 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DataAccessTier
 {
+
+    
+
     class Absentee
     {
     }
@@ -40,7 +44,81 @@ namespace DataAccessTier
 
     class MoneyTransfer { }
 
-    class Person { }
+    class Person {
+
+        DatabaseHelper db = new DatabaseHelper();
+        DataTable dt;
+
+        public DataTable SelectRecords() {
+            
+            dt = db.Select("SelectPerson");
+
+            return dt;
+
+        }
+
+        public DataTable SelectRecordswithID(int id)
+        {
+
+            dt = db.Select("SelectPersonwithID "+id);
+
+            return dt;
+
+        }
+
+        public DataTable SelectRecordswithCity(int id)
+        {
+
+            dt = db.Select("SelectPersonwithCity " + id);
+
+            return dt;
+
+        }
+
+        public DataTable SelectRecordswithName(string name)
+        {
+
+            dt = db.Select("SelectPersonwithName '" + name+"'");
+
+            return dt;
+
+        }
+
+        public DataTable SelectRecordswithMartial(string name)
+        {
+
+            dt = db.Select("SelectPersonwithMartial '" + name + "'");
+
+            return dt;
+
+        }
+
+        public DataTable SelectRecordswithReligion(string name)
+        {
+
+            dt = db.Select("SelectPersonwithReligion '" + name + "'");
+
+            return dt;
+
+        }
+
+        public void InsertPerson(string fn, string ln, string cnic, string address, int cityid, string gender, DateTime birthdate, DateTime Dateadd, string religion, string educationID, string martial, string email) {
+
+            db.insertUpdateDelete("InsertPerson '" + fn + "' '" + ln + "' '" + cnic + "' "+  address + "' " + cityid + " " + birthdate + "' '" + Dateadd + "' '" + religion + "' '" + educationID + "' '" + martial + "' '" + email + "'");
+        }
+
+        public void UpdatePerson(string fn, string ln, string cnic, string address, int cityid, string gender, DateTime birthdate, DateTime Dateadd, string religion, string educationID, string martial, string email)
+        {
+
+            db.insertUpdateDelete("UpdatePerson '" + fn + "' '" + ln + "' '" + cnic + "' " + address + "' " + cityid + " " + birthdate + "' '" + Dateadd + "' '" + religion + "' '" + educationID + "' '" + martial + "' '" + email + "'");
+        }
+
+        public void DeletePerson(int id)
+        {
+
+            db.insertUpdateDelete("DeletePerson "+id);
+        }
+    }
 
     class PhoneNumber { }
 
