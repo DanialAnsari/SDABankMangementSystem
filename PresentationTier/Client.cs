d1afbd28277786b6df37f7f7db6a32bf537c0fc5
MuhammadUsamax123 <muhammadusamax123.com>
2019-05-30 04:51:08 +0500

    Menus added

---
+++ PresentationTier/Client.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PresentationTier
+{
+    public partial class Client : MaterialSkin.Controls.MaterialForm
+    {
+        public Client()
+        {
+            InitializeComponent();
+            MaterialSkin.MaterialSkinManager skinmanager = MaterialSkin.MaterialSkinManager.Instance;
+            skinmanager.AddFormToManage(this);
+            skinmanager.Theme = MaterialSkin.MaterialSkinManager.Themes.LIGHT;
+            skinmanager.ColorScheme = new MaterialSkin.ColorScheme(MaterialSkin.Primary.Green900, MaterialSkin.Primary.BlueGrey900, MaterialSkin.Primary.Green500, MaterialSkin.Accent.Orange700, MaterialSkin.TextShade.WHITE);
+
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {   AddClient darp = new AddClient();
+            this.Hide();
+            darp.ShowDialog();
+            this.Close();
+        }
+    }
+}
---
+++ PresentationTier/Transaction.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace PresentationTier
+{
+    partial class Transaction
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Transaction));
+            this.btnAdd = new MaterialSkin.Controls.MaterialRaisedButton();
+            this.btnEdit = new MaterialSkin.Controls.MaterialRaisedButton();
+            this.btnView = new MaterialSkin.Controls.MaterialRaisedButton();
+            this.btnDel = new MaterialSkin.Controls.MaterialRaisedButton();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Depth = 0;
+            this.btnAdd.Location = new System.Drawing.Point(12, 76);
+            this.btnAdd.MouseState = MaterialSkin.MouseState.HOVER;
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Primary = true;
+            this.btnAdd.Size = new System.Drawing.Size(179, 45);
+            this.btnAdd.TabIndex = 0;
+            this.btnAdd.Text = "Add Transaction";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Depth = 0;
+            this.btnEdit.Location = new System.Drawing.Point(12, 127);
+            this.btnEdit.MouseState = MaterialSkin.MouseState.HOVER;
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Primary = true;
+            this.btnEdit.Size = new System.Drawing.Size(179, 45);
+            this.btnEdit.TabIndex = 1;
+            this.btnEdit.Text = "Edit Transaction";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            // 
+            // btnView
+            // 
+            this.btnView.Depth = 0;
+            this.btnView.Location = new System.Drawing.Point(12, 178);
+            this.btnView.MouseState = MaterialSkin.MouseState.HOVER;
+            this.btnView.Name = "btnView";
+            this.btnView.Primary = true;
+            this.btnView.Size = new System.Drawing.Size(179, 45);
+            this.btnView.TabIndex = 2;
+            this.btnView.Text = "view Transaction";
+            this.btnView.UseVisualStyleBackColor = true;
+            // 
+            // btnDel
+            // 
+            this.btnDel.Depth = 0;
+            this.btnDel.Location = new System.Drawing.Point(12, 229);
+            this.btnDel.MouseState = MaterialSkin.MouseState.HOVER;
+            this.btnDel.Name = "btnDel";
+            this.btnDel.Primary = true;
+            this.btnDel.Size = new System.Drawing.Size(179, 45);
+            this.btnDel.TabIndex = 3;
+            this.btnDel.Text = "delete Transaction";
+            this.btnDel.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(141, 26);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(60, 36);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 116;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Transaction
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(205, 284);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnDel);
+            this.Controls.Add(this.btnView);
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.btnAdd);
+            this.MaximizeBox = false;
+            this.Name = "Transaction";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Transaction";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private MaterialSkin.Controls.MaterialRaisedButton btnAdd;
+        private MaterialSkin.Controls.MaterialRaisedButton btnEdit;
+        private MaterialSkin.Controls.MaterialRaisedButton btnView;
+        private MaterialSkin.Controls.MaterialRaisedButton btnDel;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
