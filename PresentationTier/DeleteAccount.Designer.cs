5769ebfe4b2212255f6cab700e6b5ac06d4b2143
MuhammadUsamax123 <muhammadusamax123.com>
2019-05-30 03:13:42 +0500

    Delete Account Added,changes made in View Account

--- PresentationTier/DeleteAccount.Designer.cs
+++ PresentationTier/DeleteAccount.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace PresentationTier
 {
-    partial class ViewAccount
+    partial class DeleteAccount
     {
         /// <summary>
         /// Required designer variable.
@@ -28,12 +28,25 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ViewAccount));
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(DeleteAccount));
+            this.btnDelte = new MaterialSkin.Controls.MaterialRaisedButton();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // btnDelte
+            // 
+            this.btnDelte.Depth = 0;
+            this.btnDelte.Location = new System.Drawing.Point(678, 565);
+            this.btnDelte.MouseState = MaterialSkin.MouseState.HOVER;
+            this.btnDelte.Name = "btnDelte";
+            this.btnDelte.Primary = true;
+            this.btnDelte.Size = new System.Drawing.Size(88, 36);
+            this.btnDelte.TabIndex = 113;
+            this.btnDelte.Text = "Delete";
+            this.btnDelte.UseVisualStyleBackColor = true;
             // 
             // pictureBox1
             // 
@@ -43,7 +56,7 @@
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(60, 36);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureBox1.TabIndex = 109;
+            this.pictureBox1.TabIndex = 112;
             this.pictureBox1.TabStop = false;
             // 
             // dataGridView1
@@ -51,21 +64,21 @@
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Location = new System.Drawing.Point(12, 79);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(754, 519);
-            this.dataGridView1.TabIndex = 108;
+            this.dataGridView1.Size = new System.Drawing.Size(754, 480);
+            this.dataGridView1.TabIndex = 111;
             // 
-            // ViewAccount
+            // DeleteAccount
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(778, 610);
+            this.Controls.Add(this.btnDelte);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.dataGridView1);
             this.MaximizeBox = false;
-            this.Name = "ViewAccount";
+            this.Name = "DeleteAccount";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "View Account";
-            this.Load += new System.EventHandler(this.ViewAccount_Load);
+            this.Text = "Delete Account";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
@@ -74,6 +87,7 @@
 
         #endregion
 
+        private MaterialSkin.Controls.MaterialRaisedButton btnDelte;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.DataGridView dataGridView1;
     }
--- PresentationTier/ViewAccount.cs
+++ PresentationTier/ViewAccount.cs
@@ -22,5 +22,20 @@ namespace PresentationTier
             skinmanager.ColorScheme = new MaterialSkin.ColorScheme(MaterialSkin.Primary.Green900, MaterialSkin.Primary.BlueGrey900, MaterialSkin.Primary.Green500, MaterialSkin.Accent.Orange700, MaterialSkin.TextShade.WHITE);
 
         }
+
+        private void ViewAccount_Load(object sender, EventArgs e)
+        {
+            dataGridView1.BorderStyle = BorderStyle.None;
+            dataGridView1.AlternatingRowsDefaultCellStyle.BackColor = Color.FromArgb(238, 239, 249);
+            dataGridView1.CellBorderStyle = DataGridViewCellBorderStyle.SingleHorizontal;
+            dataGridView1.DefaultCellStyle.SelectionBackColor = Color.Green;
+            dataGridView1.DefaultCellStyle.SelectionForeColor = Color.WhiteSmoke;
+            dataGridView1.BackgroundColor = Color.White;
+
+            dataGridView1.EnableHeadersVisualStyles = false;
+            dataGridView1.ColumnHeadersBorderStyle = DataGridViewHeaderBorderStyle.None;
+            dataGridView1.ColumnHeadersDefaultCellStyle.BackColor = Color.FromArgb(50, 205, 50);
+            dataGridView1.ColumnHeadersDefaultCellStyle.ForeColor = Color.White;
+        }
     }
 }
