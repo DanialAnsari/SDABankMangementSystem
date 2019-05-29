237dd70f02d2d01b616b2a0a27654ab875d8fe35
MuhammadUsamax123 <muhammadusamax123.com>
2019-05-30 04:07:25 +0500

    View Transaction added

---
+++ PresentationTier/ViewTransaction.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PresentationTier
+{
+    public partial class ViewTransaction : MaterialSkin.Controls.MaterialForm
+    {
+        public ViewTransaction()
+        {
+            InitializeComponent();
+            MaterialSkin.MaterialSkinManager skinmanager = MaterialSkin.MaterialSkinManager.Instance;
+            skinmanager.AddFormToManage(this);
+            skinmanager.Theme = MaterialSkin.MaterialSkinManager.Themes.LIGHT;
+            skinmanager.ColorScheme = new MaterialSkin.ColorScheme(MaterialSkin.Primary.Green900, MaterialSkin.Primary.BlueGrey900, MaterialSkin.Primary.Green500, MaterialSkin.Accent.Orange700, MaterialSkin.TextShade.WHITE);
+
+        }
+    }
+}
